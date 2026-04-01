2ed4fe1f899e73c220f43e307072c2952505834e
adityaraj9798 <adityaraj97989@gmail.com>
2026-04-01 23:37:14 +0530

    added auth system-register,login,jwt

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import authRoutes from './routes/auth.js'
 
 dotenv.config()
 
@@ -10,6 +11,9 @@ const app = express()
 // Middleware
 app.use(cors({ origin: 'http://localhost:5173' }))
 app.use(express.json())
+
+// Routes
+app.use('/api/auth', authRoutes)
 
 // Test route
 app.get('/', (req, res) => {
