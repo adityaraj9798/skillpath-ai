86fa75d170b04a7fba8b88a59a9a7205bfb1aafb
adityaraj9798 <adityaraj97989@gmail.com>
2026-04-08 01:22:15 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
