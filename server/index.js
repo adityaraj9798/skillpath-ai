1ee296ddff3333a31b7eee35046a91d28b945979
adityaraj9798 <adityaraj97989@gmail.com>
2026-04-09 23:54:25 +0530

    changes in the file

--- server/index.js
+++ server/index.js
@@ -32,5 +32,5 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log('MongoDB error:', err))
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`))
+//app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`))
 export default app;
